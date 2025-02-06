77f8773afdf6a554c3d54c77df86c3ab55ed6398
Pedro Martins <Pedromgs23@gmail.com>
2025-02-06 17:42:00 -0300

    add tbl_ausencia
    Created a table to store employee absences, including vacations and sick leaves. The table is implemented using a list of dictionaries, where each dictionary contains:
    
    Employee name
    Start date
    End date
    Type of absence (e.g., vacation, sick leave)
    We also implemented a function to add absences to the table.

---
+++ tbl_ausencia.py
@@ -0,0 +1,11 @@
+''' tbl_ausencia '''
+ausencia = []
+
+def adicionar_ausencia(nome_funcionario, data_inicio, data_fim):
+    ausencia = {
+        'nome_funcionario': nome_funcionario,
+        'data_inicio': data_inicio,
+        'data_fim': data_fim,
+        'tipo_ausencia': tipo_ausencia
+    }
+    ausencia.append(ausencia)
